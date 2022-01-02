5411ffb33bfbd5f78c7099171f66178582b64768
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2022-01-02 12:23:55 +0700

    Update create.jsx

--- pages/blog/create.jsx
+++ pages/blog/create.jsx
@@ -1,15 +1,21 @@
 import Script from "next/script";
+import { useState } from "react";
 
 const BlogCreate = () => {
+  const [ckeditorData, setCkeditorData] = useState("Insert your content here...");
   return (
     <div>
-      <div id="editor"></div>
+      <div id="editor">{ckeditorData}</div>
       <Script
         src="/ckeditor-5/build/ckeditor.js"
         onLoad={() => {
           ClassicEditor.create(document.querySelector("#editor"))
             .then((editor) => {
-              console.log(editor);
+              // console.log(editor);
+              editor.model.document.on("change:data", () => {
+                // console.log(editor.getData());
+                setCkeditorData(editor.getData());
+              });
             })
             .catch((error) => {
               console.error(error);
